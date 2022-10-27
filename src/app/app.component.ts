4b83894263b864af46409431f8dea499ac310bd1
Ouzinem <ouzinem@hotmail.com>
2022-10-27 06:19:06 -0400

    structure training app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
+import { ITEMS } from './mock-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'trainApp';
+  title = 'Training Application for Swimmer';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems: Item[] = ITEMS;
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
 }
---
+++ src/app/mock-item.ts
@@ -0,0 +1,8 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [
+  { description: 'FreeStyle', done: true },
+  { description: 'BackStroke', done: false },
+  { description: 'Fly', done: false },
+  { description: 'BreastStroke', done: false },
+];
